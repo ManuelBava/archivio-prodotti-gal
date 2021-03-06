15ebbd3ccfbca9f785bb0b420ded8acdcf0e3207
leoIapp <leoiappelli01@gmail.com>
2021-03-06 10:56:50 +0100

    creazione cartella spring con dipendenze

---
+++ archivioProdottiGal-Server/src/main/java/it/sirfin/archivioProdottiGal/ArchivioProdottiGalApplication.java
@@ -0,0 +1,13 @@
+package it.sirfin.archivioProdottiGal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchivioProdottiGalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArchivioProdottiGalApplication.class, args);
+	}
+
+}
---
+++ archivioProdottiGal-Server/src/test/java/it/sirfin/archivioProdottiGal/ArchivioProdottiGalApplicationTests.java
@@ -0,0 +1,13 @@
+package it.sirfin.archivioProdottiGal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchivioProdottiGalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
